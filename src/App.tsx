4c7695ccbb218f7ce30347235df550bb335f6405
MonsefPRO <monsefelaidi.pro@gmail.com>
2025-12-30 14:40:52 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
     { src: '/ares.png', alt: 'Drone ARES' },
     { src: '/abateur_de_frelons.png', alt: 'Abatteur de frelons' },
     { src: '/rony2.jpg', alt: 'Expert drone en action' },
+    { src: '/chronos.jpg', alt: 'Drone Chronos' },
     { src: '/dsc03603-2.jpg', alt: 'Drone professionnel' },
     { src: '/dsc03727 copy.jpg', alt: 'Système de nettoyage en action' },
     { src: '/dsc07631 copy.jpg', alt: 'Technologie de nettoyage drone' },
