b96f6a0e322b25c00bdf4463896edd73e289d9f1
MonsefPRO <monsefelaidi.pro@gmail.com>
2026-01-02 12:15:26 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -97,13 +97,13 @@ function App() {
       <CookieBanner />
       <header className="fixed w-full bg-white/98 backdrop-blur-md shadow-md z-40 transition-all border-b border-gray-100">
         <nav className="w-full">
-          <div className="flex justify-between items-center h-[110px]">
+          <div className="flex justify-between items-center h-[110px] px-5">
             <div className="flex items-center">
               <Link to="/" className="flex-shrink-0">
                 <img
                   src="/logo_de_cote.png"
                   alt="Ellipsys"
-                  className='h-28 sm:h-36 md:h-44 lg:h-56 xl:h-64 2xl:h-72 w-auto -ml-4 hover:scale-110 transition-transform duration-300 cursor-pointer'
+                  className='h-28 sm:h-36 md:h-44 lg:h-56 xl:h-64 2xl:h-72 w-auto hover:scale-110 transition-transform duration-300 cursor-pointer'
                 />
               </Link>
             </div>
