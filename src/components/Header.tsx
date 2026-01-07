33625fc9bd8884660ab67a47bbc3a75f7821bd31
MonsefPRO <monsefelaidi.pro@gmail.com>
2026-01-07 10:31:01 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ export default function Header({ onBlueBanner = false }: HeaderProps) {
           <div className="flex items-center relative h-full">
   <Link to="/" className="flex items-center absolute left-0">
     <img 
-      src="/bon_logo_de_cote.png" 
+      src="/bonlogo_de_cote.png" 
       alt="Ellipsys Logo"
       className="h-24 md:h-36 w-auto object-contain transition-transform hover:scale-105 drop-shadow-md"
       style={{ maxWidth: 'none' }}
